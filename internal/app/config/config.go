6a948eda861b3a0817e96820835675c1508a16e8
Alex Lagson <zerocoolby@gmail.com>
2026-03-25 22:44:55 +0100

    TASK1000/1001:реализовал загрузку конфигурации из .env файла

---
+++ internal/app/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+
+	"github.com/Lagwick/catalog-service/internal/app/config/section"
+)
+
+type Config struct {
+	Repository section.Repository `split_words:"true"`
+	Processor  section.Processor  `split_words:"true"`
+	Monitor    section.Monitor    `split_words:"true"`
+}
+
+var Root Config
+
+func Load() {
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env file not found")
+	}
+	err := envconfig.Process("APP", &Root)
+	if err != nil {
+		log.Fatalf("config parse error: %v", err)
+	}
+}
---
+++ internal/app/util/duration.go
@@ -0,0 +1,17 @@
+package util
+
+import "time"
+
+type Duration struct {
+	time.Duration
+}
+
+func (d *Duration) UnmarshalText(text []byte) error {
+	res, err := time.ParseDuration(string(text))
+	if err != nil {
+
+		return err
+	}
+	d.Duration = res
+	return nil
+}
