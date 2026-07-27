380ebbcab1233fe8631425f33b122b2f16f1f5d9
Alex Lagson <zerocoolby@gmail.com>
2026-07-27 22:50:56 +0200

    TASK-4001: подготовили базовый слой наблюдаемости grafana+prometheus

--- internal/app/processor/http/api_generic.go
+++ internal/app/processor/http/api_generic.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	rhandler "github.com/Lagwick/catalog-service/internal/app/handler/http"
 )
 
 func vGenericRegHealthCheck(r *mux.Router, h rhandler.Health) {
 	reg(r, http.MethodGet, "/health", http.HandlerFunc(h.LastCheck))
+	reg(r, http.MethodGet, "/metrics", promhttp.Handler())
 }
 
 func handlerNotFound(w http.ResponseWriter, _ *http.Request) {
